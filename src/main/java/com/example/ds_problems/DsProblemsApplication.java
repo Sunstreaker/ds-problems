a121cb0e14853fb725d134d0fd9ea1852ea96791
Sunstreaker <suthagar.pp@gmail.com>
2024-07-17 21:13:34 +0530

    initial commit

---
+++ src/main/java/com/example/ds_problems/DsProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ds_problems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ds_problems/DsProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ds_problems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
